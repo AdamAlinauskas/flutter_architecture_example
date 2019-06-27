eda34206a8971cdd09573db80cabf5fbb896daa3
Adam <aalinauskas@gmail..com>
2019-06-26 20:57:11 -0600

    Add some style

--- lib/app.dart
+++ lib/app.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo/src/controllers/todo_controller.dart';
-import 'package:todo/src/data_access/todo_repository.dart';
 import 'package:todo/src/ioc_container.dart';
 import 'package:todo/src/routes/router.dart';
-import 'package:get_it/get_it.dart';
 
-import 'main.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       child: MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+          buttonTheme: ButtonThemeData(textTheme: ButtonTextTheme.normal)
+        ),
+
         title: 'todo',
         onGenerateRoute: Router().route,
       ),
